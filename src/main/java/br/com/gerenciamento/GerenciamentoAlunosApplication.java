bbb2715152d867d3f690151b09de21ff5cb8a002
rafinhads <rafadiasnascentes@gmail.com>
2025-03-17 18:46:01 -0300

    Alteração das cores do menu e ADICIONADO O BOTÃO DE GERAR PDF

--- src/main/java/br/com/gerenciamento/GerenciamentoAlunosApplication.java
+++ src/main/java/br/com/gerenciamento/GerenciamentoAlunosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciamentoAlunosApplication {
+public class    GerenciamentoAlunosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciamentoAlunosApplication.class, args);
